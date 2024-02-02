9a334457a4293ef52effe318e224cae86a5f6b99
PellanG <pellan.gustafsson@hotmail.com>
2024-02-02 10:22:12 +0100

    fixed initial styling on animals

--- src/components/AnimalPresentation.tsx
+++ src/components/AnimalPresentation.tsx
@@ -5,8 +5,13 @@ interface IAnimalPresentationProps{
 }
 
 export const AnimalPresentation = ({animal}:IAnimalPresentationProps) => {  
-        return<div>
-        <h2>{animal.name}</h2>;
-        <img src={animal.imageUrl}/>;
+        return<>
+        <div className="animals-container">
+        <h2>{animal.name}</h2>
+        <div className="img-container">
+        <img src={animal.imageUrl}/>
         </div>
+        </div></>
+      
+        
 }
